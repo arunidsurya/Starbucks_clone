573fcff2bc8f6098e5970cfe03de4ac2c24d1c66
Arun <arunps1011@gmail.com>
2023-08-07 22:12:02 +0200

    navigation and header added

---
+++ index.js
@@ -0,0 +1,8 @@
+function imgSlider(changeImage){
+    document.querySelector('.starbucks').src= changeImage;
+}
+
+function changeCircleColor(color){
+   const circle = document.querySelector('.circle');
+   circle.style.background=color;
+}
